35969c0d77cba0422063045c0da5b79c4633de9c
Dev-Farrukh <m.farrukhnoman@gmail.com>
2026-06-07 20:29:20 +0500

    final fix in frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -83,7 +83,7 @@ db_config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://genai-resume-enhancer.vercel.app/",
+    origin: "https://genai-resume-enhancer.vercel.app",
     credentials: true
 }))
 
